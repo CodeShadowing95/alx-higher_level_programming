54a382047a6dad851ea05237c0acc44df949c0f5
Frank Patrick <ksenbonsakura@gmail.com>
2022-06-08 16:02:10 -0700

    New commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = list()
-    if matrix:
-        for i in range(len(matrix)):
-            new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
-
-    return new_matrix
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,3 +1,2 @@
 <h1>Python Programming</h1>
-<h2>More data structures: Set, Dictionary</h2>
-0- print the square value of all integers of a matrix</br>
+
