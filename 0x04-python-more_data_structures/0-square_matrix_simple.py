d57092780ba0bd6c82ebbefaff12785b3b044de3
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-08 16:47:04 -0700

    square integers matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix2 = []
+    for i in range(len(matrix)):
+        matrix2.append(list(map(lambda x: x ** 2, matrix[i])))
+
+    return matrix2
+
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,2 +1,3 @@
 <h1>Python Programming</h1>
-
+<h2>More data structures: Set, Dictionary</h2>
+0- get the square value of all integers of a matrix</br>
