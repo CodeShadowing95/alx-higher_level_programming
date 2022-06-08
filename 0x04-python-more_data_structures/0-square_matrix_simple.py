49aeafc1d5a86cec305121764760fbb51d321aad
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-08 16:53:22 -0700

    square matrix integers

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix2 = []
+    matrix2 = matrix.copy()
+
     for i in range(len(matrix)):
-        matrix2.append(list(map(lambda x: x ** 2, matrix[i])))
+        matrix2[i] = list(map(lambda x: x ** 2, matrix[i]))
 
     return matrix2
 
