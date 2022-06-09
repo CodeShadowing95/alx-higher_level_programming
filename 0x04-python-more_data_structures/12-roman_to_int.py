0e41b83f05c49876a3441538f81c4530ee7ddef5
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-09 11:21:38 -0700

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,24 @@
 from functools import reduce
 from sre_compile import isstring
 
-roman_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 def roman_to_int(roman_string):
+    roman_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
     c = 0
 
-    if (not isstring(roman_string)) or roman_string == None:
+    if (not isstring(roman_string)) or roman_string is None:
         return 0
 
-    nbrs = [roman_int[roman_nbr] for roman_nbr in roman_string if roman_nbr in list(roman_int.keys())]
+    nbrs = [roman_int[i] for i in roman_string if i in list(roman_int.keys())]
 
-    c = reduce(lambda a,b: -1 * (a - b) if a < b else a + b, nbrs)
+    c = reduce(lambda a, b: -1 * (a - b) if a < b else a + b, nbrs)
 
     return c
