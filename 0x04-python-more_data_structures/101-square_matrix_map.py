50601570ce56a972f0d32671cf858e14046cdf14
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-19 06:35:31 -0700

    squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda x: x, map(lambda x: [x[0]**2, x[1]**2, x[2]**2], matrix)))
+def square_matrix_map(matrix=[]): return list(map(
+    lambda x: x, map(lambda x: [x[0]**2, x[1]**2, x[2]**2], matrix)))
