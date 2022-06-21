aff89cdf5a70822350d4e5e321e962db2cabecfa
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-21 11:25:54 -0700

    square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    class Square:
-        """The square object represents the square of a any number"""
-        pass  # An empty block
+
+class Square:
+    """The square object represents the square of a any number"""
+    pass
+
+help(Square)
