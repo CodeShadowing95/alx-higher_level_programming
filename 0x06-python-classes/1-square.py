31fe4c9482d18dfcfbb841e2730b83c1a26e51f2
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-22 05:55:13 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Definition of the class Square by adding some attributes"""
+
+
+class Square:
+    """
+    A class to represent any square.
+
+
+    ...
+
+    Attributes
+    ----------
+    size : int
+        size of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size  # private
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,1 +1,4 @@
 <h1>Python - Classes and Objects</h1>
+<h2>Examples</h2>
+0- create an empty class Square</br>
+1- add to class Square the private instance attribute <code>size</code></br>
