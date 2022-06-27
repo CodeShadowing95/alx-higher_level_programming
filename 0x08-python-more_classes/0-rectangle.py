bfe521900ed4c1a67b3898aa112396c810b0308d
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-27 12:15:59 -0700

    empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Building of a class Rectangle defining a rectangle"""
+
+
+class Rectangle:
+    """Empty class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+<h1>Python - More classes</h1>
+<h2>Examples</h2>
+0- create an empty class Rectangle that defines a rectangle</br>
