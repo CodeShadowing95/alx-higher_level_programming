d405ab19f2382b86a8cb340b73260c9db194a18b
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-06-28 00:15:10 -0700

    compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -161,5 +161,5 @@ class Rectangle:
             return rect_1
         if rect_1.area() <= rect_2.area():
             return rect_2
-        if rect_1.area() == rect_2.area()
+        if rect_1.area() == rect_2.area():
             return rect_1
