2835f33d7e21545b0c982b54bfa6825f19d203de
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-04 16:22:53 -0700

    improve geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,7 +4,6 @@
 
 class BaseGeometry:
     """A class to represent any geometry"""
-    
     def area(self):
         """raise an Exception with a message"""
         raise Exception("area() is not implemented")
