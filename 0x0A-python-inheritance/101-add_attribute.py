707dedf886c497be4f5709ed72ae909a6b14daa8
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-05 05:26:33 -0700

    add new attribute to object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""definition of the function add_attribute()"""
+
+
+def add_attribute(obj, name, value):
+    """function add_attribute
+    Args:
+        obj: object's name in which attributes will be set
+        name: name of the attibute of that object that we want to assign value
+        value: the attibute value
+    """
+    if type(obj) in [int, bool, float, complex, str, list, tuple, set, dict, None]:
+        raise TypeError("can't add new attibute")
+    else:
+        setattr(obj, name, value)
