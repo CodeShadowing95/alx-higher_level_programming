fe4d1ddefc4c39e079c2ab8bc2b689b7765d8ed4
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-05 05:52:37 -0700

    new changes

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,7 +9,8 @@ def add_attribute(obj, name, value):
         name: name of the attibute of that object that we want to assign value
         value: the attibute value
     """
-    if type(obj) in [int, bool, float, complex, str, list, tuple, set, dict, None]:
+    types = [int, bool, float, complex, str, list, tuple, set, dict, None:]
+    if type(obj) in types:
         raise TypeError("can't add new attibute")
     else:
         setattr(obj, name, value)
