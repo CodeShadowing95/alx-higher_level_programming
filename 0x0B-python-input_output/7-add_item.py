be276c069318deec71038f8c924bc6f7c0c0ad23
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-06 15:36:03 -0700

    Some changes

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Definition of the function add_item()"""
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(filename):
+    """add all arguments to a Python list, and save them to a file
+
+    Args:
+        filename: JSON file in which the arguments list will be saved
+    """
+    my_args = sys.argv[1:]
+    if len(my_args) == 0:
+        save_to_json_file(my_args, filename)
+    else:
+        tab = load_from_json_file(filename)
+        for i in my_args:
+            tab.append(i)
+        save_to_json_file(tab, filename)
+
+
+add_item("add_item.json")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -56,3 +56,4 @@ Convert a JSON string to a Python data structure, using the <code>json.loads(dat
 4- function that returns an object (Python data structure) represented by a JSON string</br>
 5- function that writes an Object to a text file, using a JSON representation</br>
 6- function that creates an Object from a "JSON file"</br>
+7- scirpt that adds all arguments to a Python list, and then save them to a file</br>
