f973c5472a184fa5f7358cd54edb4709ea9b43e4
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-06 07:06:35 -0700

    new change

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,12 +4,12 @@
 
 def add_attribute(obj, attibute, value):
     """function add_attribute
-    
+
     Args:
         obj: object's name in which attributes will be set
         name: name of the attibute of that object that we want to assign value
         value: the attibute value
-    
+
     Raises:
         TypeError if cannot add attribute
     """
