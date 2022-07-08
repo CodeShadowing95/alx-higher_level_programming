40f39595dc3022f45ce33905a5d7feba7ec90827
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-08 13:07:02 -0700

    Some changes

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,7 +9,23 @@ def pascal_triangle(n):
     Args:
         n: number of lists of lists of integer
     """
+    tab = []
+    s = []
+    j = 0
     if n <= 0:
         return []
     for i in range(n):
-        
+        if i < 1:
+            temp = [1]
+            tab.append(temp)
+        else:
+            s.append(temp[0])
+            for i in range(len(temp)-1):
+                j = i + 1
+                if j != len(temp):
+                    s.append(temp[i] + temp[j])
+            s.append(temp[j])
+            tab.append(s)
+            temp = s
+            s = []
+    return tab
