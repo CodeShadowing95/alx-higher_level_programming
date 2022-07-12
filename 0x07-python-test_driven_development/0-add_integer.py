0bf3b0b5b30fa9a9d3db337bd5dc724f2da13e24
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-07-12 10:46:36 -0700

    Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""doctest_add_integer.py
+"""
+doctest_add_integer.py
+
+The 0-add_integer module supplies one function, add_integer().
 
 >>> add_integer(2, 9)
 11
@@ -18,6 +21,10 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
+    elif type(a) == float:
+        a = int(a)
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    elif type(b) == float:
+        b = int(b)
+    return a + b
