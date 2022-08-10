c8a84df700ca6207bed598de1754a3fa09de39cc
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-08-09 19:25:12 -0700

    SQL query commit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- Script that converts a database to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
