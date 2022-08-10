b29ef5468f1788ed8d169aaa40ea95dc9f4878d1
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-08-09 17:38:43 -0700

    SQL query commit

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- Script that lists all the tables of a database
-SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Script that creates a table in the current database
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))
