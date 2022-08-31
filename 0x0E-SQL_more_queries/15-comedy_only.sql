b4fc812551c4cce07a88803c30ba64c9d2773e82
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-08-30 22:27:31 -0700

    More SQL queries commit

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- List all Comedy shows
+SELECT tv_shows.title FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title;
