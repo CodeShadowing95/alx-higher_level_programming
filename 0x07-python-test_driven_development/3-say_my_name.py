72e3580d496402704f4e7cc5da181eda21b80413
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-09-04 14:34:24 -0700

    Test-driven development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+"""
+doctest_say_my_name
+
+This function prints My name is <first name> <last name>
+
+>>> say_my_name("John", "Smith")
+My name is John Smith
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Returns the first name and last name
+
+    Params:
+        first_name: string
+        last_name: string
+
+    Raises:
+        TypeError if first_name or last_name are not strings
+    """
+    e1 = "first_name must be a string"
+    e2 = "last_name must be a string"
+    if not isinstance(first_name, str):
+        raise TypeError(e1)
+    if not isinstance(last_name, str):
+        raise TypeError(e2)
+    print("My name is {} {}".format(first_name, last_name))
