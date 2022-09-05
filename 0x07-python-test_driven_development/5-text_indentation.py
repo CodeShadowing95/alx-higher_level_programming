8e4af5246e00faa8c58195b43823245618c61125
CodeShadowing95 <ksenbonsakura@gmail.com>
2022-09-04 18:13:33 -0700

    Test-driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+doctest_text_indentation
+
+Print a text with 2 new lines after each of these characters: . and ? and :
+
+>>> text_indentation("Lorem ipsum. Dolor sit amet")
+Lorem ipsum
+
+Dolor sit amet
+"""
+
+
+def text_indentation(text):
+    """Return a text with 2 new lines after the characters ., ? and :
+
+    Params:
+        text: string
+
+    Raises:
+        TypeError if text is not a string
+    """
+    c = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if i in ".?:":
+            sc = i
+            c = 1
+        elif c == 1 and i == " ":
+            print(sc + "\n")
+            c = 0
+        else:
+            print(i, end="")
